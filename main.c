503c425d9c0daad4e146c2279c6c6d896bb9c357
MarcHamamji <hamamjimarc@gmail.com>
2024-02-16 20:13:17 +0200

    Improve performance by using PixBufs

--- main.c
+++ main.c
@@ -1,19 +1,45 @@
-#include "cairo.h"
+#include <cairo.h>
 #include <complex.h>
 #include <gtk/gtk.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-const int size = 800;
+#define SIZE 800
 
 bool julia = false;
 double complex julia_z0 = 0 + 0.5 * I;
 
 int max_iter = 40;
-double scale = 400;
-double complex center = size * 0.7 + (size / 2.0) * I;
+double complex_width = 3;
+double complex complex_screen_center = 0;
 
 GtkWidget *drawing_area;
+
+guchar *data;
+
+static double map(double x, double fromMin, double fromMax, double toMin,
+                  double toMax) {
+  return (x - fromMin) * (toMax - toMin) / (fromMax - fromMin) + toMin;
+}
+
+static double complex complex_to_screen(double complex z, double complex center,
+                                        double width) {
+  double x = map(creal(z), creal(center) - width / 2, creal(center) + width / 2,
+                 0, SIZE);
+  double y = map(cimag(z), cimag(center) - width / 2, cimag(center) + width / 2,
+                 SIZE, 0);
+  return x + I * y;
+}
+
+static double complex screen_to_complex(double complex z, double complex center,
+                                        double width) {
+  double x = map(creal(z), 0, SIZE, creal(center) - width / 2,
+                 creal(center) + width / 2);
+  double y = map(cimag(z), 0, SIZE, cimag(center) + width / 2,
+                 cimag(center) - width / 2);
+  return x + I * y;
+}
 
 static int8_t diverging_threshold(double complex initial_z, double complex c,
                                   int max_iter) {
@@ -27,51 +53,60 @@ static int8_t diverging_threshold(double complex initial_z, double complex c,
   return -1;
 }
 
-double complex mapped_coordinates(double complex position,
-                                  double complex center, double scale) {
-  return conj((position - center) / scale);
-}
-
-double complex reverse_mapped_coordinates(double complex position,
-                                          double complex center, double scale) {
-  return conj(position) * scale + center;
-}
-
 void color_point(cairo_t *cr, int x, int y, int width, int height) {
-  double complex complex_coords = mapped_coordinates(x + y * I, center, scale);
+  double complex complex_point =
+      screen_to_complex(x + y * I, complex_screen_center, complex_width);
   int8_t threshold;
+
   if (julia) {
-    threshold = diverging_threshold(complex_coords, julia_z0, max_iter);
+    threshold = diverging_threshold(complex_point, julia_z0, max_iter);
   } else {
-    threshold = diverging_threshold(0, complex_coords, max_iter);
+    threshold = diverging_threshold(0, complex_point, max_iter);
   }
+
+  double color;
   if (threshold == -1) {
-    cairo_set_source_rgb(cr, 0, 0, 0);
+    color = 0;
   } else {
-    double color = threshold / 20.0;
-    cairo_set_source_rgb(cr, color, color, color);
+    color = threshold * 255.0 / 20;
+    if (color > 255)
+      color = 255;
   }
-  cairo_rectangle(cr, x, y, 1, 1);
-  if (!julia && 2 * cimag(center) - y < height && 2 * cimag(center) - y >= 0) {
-    cairo_rectangle(cr, x, 2 * cimag(center) - y, 1, 1);
+
+  data[(y * width + x) * 3] = color;
+  data[(y * width + x) * 3 + 1] = color;
+  data[(y * width + x) * 3 + 2] = color;
+  if (!julia && 2 * cimag(complex_screen_center) - y < height &&
+      2 * cimag(complex_screen_center) - y >= 0) {
+    data[(int)((2 * cimag(complex_screen_center) - y) * width + x) * 3] = color;
+    data[(int)((2 * cimag(complex_screen_center) - y) * width + x) * 3 + 1] =
+        color;
+    data[(int)((2 * cimag(complex_screen_center) - y) * width + x) * 3 + 2] =
+        color;
   }
-  cairo_fill(cr);
 }
 
 static void draw_axes(cairo_t *cr) {
   cairo_set_source_rgb(cr, 0.5, 1, 0.8);
   cairo_set_line_width(cr, 2);
-  cairo_move_to(cr, creal(center), 0);
-  cairo_line_to(cr, creal(center), size);
-  cairo_move_to(cr, 0, cimag(center));
-  cairo_line_to(cr, size, cimag(center));
+
+  double complex screen_center =
+      complex_to_screen(0, complex_screen_center, complex_width);
+  cairo_move_to(cr, creal(screen_center), 0);
+  cairo_line_to(cr, creal(screen_center), SIZE);
+  cairo_move_to(cr, 0, cimag(screen_center));
+  cairo_line_to(cr, SIZE, cimag(screen_center));
+
   cairo_stroke(cr);
 }
 
 static void draw_julia_z0(cairo_t *cr) {
   cairo_set_source_rgb(cr, 0.5, 1, 0.8);
   cairo_set_line_width(cr, 2);
-  double complex z0 = reverse_mapped_coordinates(julia_z0, center, scale);
+
+  double complex z0 =
+      complex_to_screen(julia_z0, complex_screen_center, complex_width);
+
   cairo_arc(cr, creal(z0), cimag(z0), 5, 0, 2 * M_PI);
   cairo_fill(cr);
 
@@ -83,18 +118,18 @@ static void draw_julia_z0(cairo_t *cr) {
 }
 
 static void draw(GtkDrawingArea *drawing_area, cairo_t *cr, int width,
-                          int height, gpointer data) {
+                 int height, gpointer _data) {
 
   if (!julia) {
-    if (cimag(center) > height / 2.0) {
+    if (cimag(complex_screen_center) > height / 2.0) {
       for (int x = 0; x < width; x++) {
-        for (int y = 0; y <= cimag(center); y++) {
+        for (int y = 0; y <= cimag(complex_screen_center); y++) {
           color_point(cr, x, y, width, height);
         }
       }
     } else {
       for (int x = 0; x < width; x++) {
-        for (int y = cimag(center); y < height; y++) {
+        for (int y = cimag(complex_screen_center); y < height; y++) {
           color_point(cr, x, y, width, height);
         }
       }
@@ -107,54 +142,60 @@ static void draw(GtkDrawingArea *drawing_area, cairo_t *cr, int width,
     }
   }
 
+  gdk_cairo_set_source_pixbuf(cr,
+                              gdk_pixbuf_new_from_data(data, GDK_COLORSPACE_RGB,
+                                                       FALSE, 8, width, height,
+                                                       width * 3, NULL, NULL),
+                              0, 0);
+  cairo_paint(cr);
+
   draw_axes(cr);
   if (julia) {
     draw_julia_z0(cr);
   }
 }
 
-
 static gboolean on_key_pressed(GtkEventControllerKey *controller, guint keyval,
                                guint keycode, GdkModifierType state) {
 
   switch (keyval) {
   case GDK_KEY_Up:
-    center += 50 * I;
+    complex_screen_center += 0.1 * complex_width * I;
     gtk_widget_queue_draw(drawing_area);
     break;
   case GDK_KEY_Down:
-    center -= 50 * I;
+    complex_screen_center -= 0.1 * complex_width * I;
     gtk_widget_queue_draw(drawing_area);
     break;
   case GDK_KEY_Right:
-    center -= 50;
+    complex_screen_center += 0.1 * complex_width;
     gtk_widget_queue_draw(drawing_area);
     break;
   case GDK_KEY_Left:
-    center += 50;
+    complex_screen_center -= 0.1 * complex_width;
     gtk_widget_queue_draw(drawing_area);
     break;
   case GDK_KEY_plus:
-  case GDK_KEY_equal: {
-    scale += 50;
-    max_iter += 1;
+  case GDK_KEY_equal:
+    complex_width *= 0.9;
+    max_iter += 2;
     gtk_widget_queue_draw(drawing_area);
     break;
-  }
   case GDK_KEY_minus:
-    scale -= 50;
-    max_iter -= 1;
+    complex_width *= 1.1;
+    max_iter -= 2;
     gtk_widget_queue_draw(drawing_area);
     break;
   case GDK_KEY_q:
+    free(data);
     exit(0);
   case GDK_KEY_j:
     julia = !julia;
     gtk_widget_queue_draw(drawing_area);
     break;
   case GDK_KEY_h:
-    center = size * 0.5 + (size * 0.5) * I;
-    scale = 400;
+    complex_screen_center = 0;
+    complex_width = 3;
     gtk_widget_queue_draw(drawing_area);
     break;
   case GDK_KEY_w:
@@ -181,15 +222,17 @@ static gboolean on_key_pressed(GtkEventControllerKey *controller, guint keyval,
 static void activate(GtkApplication *app, gpointer user_data) {
   GtkWidget *window;
 
+  data = malloc(SIZE * SIZE * 3);
+
   window = gtk_application_window_new(app);
   gtk_window_set_title(GTK_WINDOW(window), "Fractales");
-  gtk_window_set_default_size(GTK_WINDOW(window), size, size);
+  gtk_window_set_default_size(GTK_WINDOW(window), SIZE, SIZE);
   gtk_window_set_resizable(GTK_WINDOW(window), false);
 
   drawing_area = gtk_drawing_area_new();
   gtk_window_set_child(GTK_WINDOW(window), drawing_area);
-  gtk_drawing_area_set_draw_func(GTK_DRAWING_AREA(drawing_area), draw,
-                                 NULL, NULL);
+  gtk_drawing_area_set_draw_func(GTK_DRAWING_AREA(drawing_area), draw, NULL,
+                                 NULL);
 
   GtkEventController *event_controller;
   event_controller = gtk_event_controller_key_new();
